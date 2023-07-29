cec62494b95b12d07ce1eae4ff5820b14cbffad3
Paulo555Bispo <pbispo555@gmail.com>
2023-07-28 21:23:39 -0300

    Primeiro commit

---
+++ src/com/algaworks/banco/modelo/Pessoa.java
@@ -0,0 +1,25 @@
+package com.algaworks.banco.modelo;
+
+import jdk.jfr.SettingDefinition;
+
+public class Pessoa {
+
+    private String nome;
+    private String documento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+}
